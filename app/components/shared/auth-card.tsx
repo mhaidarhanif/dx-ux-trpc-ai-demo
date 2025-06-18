db4be2d28f6022b6dc6a0da2075880ec9f6dae33
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-18 22:36:44 +0700

    feat: update auth card layout and button text for improved user experience

--- app/components/shared/auth-card.tsx
+++ app/components/shared/auth-card.tsx
@@ -3,7 +3,6 @@ import { Form, href, Link } from "react-router";
 
 import { Logo } from "@/components/shared/logo";
 import { Button } from "@/components/ui/button";
-import { CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth/client";
@@ -30,72 +29,80 @@ export function AuthCard({
     });
   };
 
+  const isSignIn = mode === "signin";
+  const buttonText = isSignIn ? "Sign In" : "Sign Up";
+  const buttonCTA = isSignIn ? "Continue with Email" : "Create New Account";
+
   return (
-    <section className={cn("flex flex-col gap-6", className)} {...props}>
+    <section
+      className={cn("flex flex-col items-center gap-6", className)}
+      {...props}
+    >
       <div className="flex self-center">
         <Logo classNameText="font-black font-brand" />
       </div>
 
-      <div className="bg-none">
-        <CardContent className="grid w-full max-w-md gap-6">
-          <div className="flex flex-col gap-4">
-            <Button variant="outline" onClick={() => signInGitHub()}>
-              <SiGithub />
-              <span>Sign in with GitHub</span>
-            </Button>
-            <Button variant="outline" onClick={() => signInGoogle()}>
-              <SiGoogle />
-              <span>Sign in with Google</span>
-            </Button>
-          </div>
+      <div className="grid w-full gap-6">
+        <div className="flex flex-col gap-4">
+          <Button variant="outline" onClick={() => signInGitHub()}>
+            <SiGithub />
+            <span>{buttonText} with GitHub</span>
+          </Button>
+          <Button variant="outline" onClick={() => signInGoogle()}>
+            <SiGoogle />
+            <span>{buttonText} with Google</span>
+          </Button>
+        </div>
 
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-border after:border-t">
+          <span className="relative z-10 bg-background px-2 text-muted-foreground">
+            or
+          </span>
+        </div>
 
-          <Form className="grid gap-6">
-            <div className="grid gap-3">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="email@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-3">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  to="/forgot-password"
-                  className="ml-auto text-xs underline-offset-4 hover:underline"
-                >
+        <Form className="grid gap-6">
+          <div className="grid gap-3">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="email@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-3">
+            <div className="flex items-center justify-between">
+              <Label htmlFor="password">Password</Label>
+              {mode === "signin" && (
+                <Link to="/forgot-password" className="text-xs">
                   Forgot your password?
                 </Link>
-              </div>
-              <Input id="password" type="password" required />
+              )}
             </div>
-            <Button type="submit" className="w-full">
-              Sign In
-            </Button>
-          </Form>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            {buttonCTA}
+          </Button>
+        </Form>
 
+        {isSignIn && (
           <div className="text-center text-sm">
             Don&apos;t have an account?{" "}
             <Link to={href("/signup")} className="underline underline-offset-4">
               Sign up
             </Link>
           </div>
-        </CardContent>
+        )}
       </div>
 
-      <div className="max-w-xs text-pretty text-center text-muted-foreground text-xs *:[a]:underline *:[a]:underline-offset-4 *:[a]:hover:text-primary">
-        By clicking continue, you agree to our{" "}
-        <Link to={href("/about")}>Terms of Service</Link> and{" "}
-        <Link to={href("/about")}>Privacy Policy</Link>.
-      </div>
+      {!isSignIn && (
+        <div className="max-w-xs text-pretty text-center text-muted-foreground text-xs *:[a]:underline *:[a]:underline-offset-4 *:[a]:hover:text-primary">
+          By clicking continue, you agree to our{" "}
+          <Link to={href("/about")}>Terms of Service</Link> and{" "}
+          <Link to={href("/about")}>Privacy Policy</Link>.
+        </div>
+      )}
     </section>
   );
 }
