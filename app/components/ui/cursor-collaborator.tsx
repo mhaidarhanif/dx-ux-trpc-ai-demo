fc530e0eecf73a78fe3918544b22f8f4e4d07e93
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-25 05:00:57 +0700

    fix: svg www.w3.org

--- app/components/ui/cursor-collaborator.tsx
+++ app/components/ui/cursor-collaborator.tsx
@@ -23,7 +23,7 @@ export const CursorPointer = ({ className, ...props }: CursorPointerProps) => (
     height="20"
     viewBox="0 0 20 20"
     width="20"
-    xmlns="http://w3.org/2000/svg"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
