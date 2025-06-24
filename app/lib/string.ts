59d8d235ba0a82b2811cd985367a8fa3be0e6194
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-25 03:27:22 +0700

    fix: getNameParts if only 1 word

--- app/lib/string.ts
+++ app/lib/string.ts
@@ -102,6 +102,12 @@ export function getNameInitials(fullname = "First Last") {
 
 export function getNameParts(fullname: string) {
   const parts = fullname.split(" ");
+  if (parts.length === 1) {
+    return {
+      firstName: parts[0],
+      lastName: "",
+    };
+  }
   return {
     firstName: parts.slice(0, -1).join(" "),
     lastName: parts.slice(-1).join(" "),
