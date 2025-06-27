7e837755f0647bec134906d771bd2d34bc3b1fd7
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-27 21:36:42 +0700

    fix: zod import

--- app/server/routers/example.ts
+++ app/server/routers/example.ts
@@ -1,5 +1,5 @@
 import type { TRPCRouterRecord } from "@trpc/server";
-import z from "zod/v4-mini";
+import { z } from "zod/v4-mini";
 import { configPrismaCache } from "@/config/prisma-cache";
 import { publicProcedure } from "@/server/trpc";
 
