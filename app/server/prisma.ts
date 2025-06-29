3a2a7f80fe6e4e7a58d4d066cf83884988c21692
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-29 21:06:31 +0700

    feat: revert prisma config

--- app/server/prisma.ts
+++ app/server/prisma.ts
@@ -1,12 +1,8 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    adapter,
     // log: ["query"],
   }).$extends(withAccelerate());
 };
