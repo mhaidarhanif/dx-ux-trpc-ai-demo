b8ecf3976f35a7fc4e59bbe3b2411eb26c88a24c
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-29 19:26:03 +0700

    chore: try to optimize in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,32 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./app") },
+  },
   server: {
     port: Number(process.env.PORT) || 8000,
   },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./app"),
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
+  build: {
+    cssCodeSplit: true,
+    minify: "esbuild",
+    sourcemap: false,
+    target: "esnext",
+    commonjsOptions: {
+      include: [/node_modules/],
     },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
+  optimizeDeps: {
+    exclude: ["react", "react-dom"],
+  },
+  define: {
+    __DEV__: process.env.NODE_ENV !== "production",
   },
 });
