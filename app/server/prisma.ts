971bda86c0245c69e277c66460983d8ba5aa51a9
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-29 21:14:11 +0700

    chore: build remove check, comments on prisma config

--- app/server/prisma.ts
+++ app/server/prisma.ts
@@ -1,6 +1,12 @@
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "@/generated/prisma/client";
 
+/**
+ * Enable custom adapter, ONLY if not using Prisma Accelerate
+ */
+// import { PrismaPg } from "@prisma/adapter-pg";
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
 const prismaClientSingleton = () => {
   return new PrismaClient({
     // log: ["query"],
