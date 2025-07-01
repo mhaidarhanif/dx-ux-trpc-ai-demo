e828cf36d43d075e1e0a99341bd032a396075778
M Haidar Hanif <git@mhaidarhanif.com>
2025-07-02 02:49:24 +0700

    refactor: currencies

--- app/components/ui/currency-select.tsx
+++ app/components/ui/currency-select.tsx
@@ -15,7 +15,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { allCurrencies, customCurrencies } from "@/constants/currencies";
+import { allCurrencies, customCurrencies } from "@/lib/currencies";
 import { cn } from "@/lib/utils";
 
 export interface Currency {
