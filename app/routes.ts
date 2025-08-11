23b6b4ddbb3b9d3a840e4b5242eb487a7442176a
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-11 14:52:30 +0700

    feat: routes prefix api action

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import {
   index,
   layout,
+  prefix,
   type RouteConfig,
   route,
 } from "@react-router/dev/routes";
@@ -9,26 +10,30 @@ export default [
   layout("web/layouts/layout-app.tsx", [
     layout("web/layouts/layout-padded.tsx", [
       index("web/home.tsx"),
-      route("/about", "web/about.tsx"),
-      route("/examples", "web/examples.tsx"),
-      route("/examples/:slug", "web/examples-slug.tsx"),
+      route("about", "web/about.tsx"),
+      route("examples", "web/examples.tsx"),
+      route("examples/:slug", "web/examples-slug.tsx"),
     ]),
 
     layout("web/layouts/layout-auth.tsx", [
-      route("/signup", "web/signup.tsx"),
-      route("/signin", "web/signin.tsx"),
-      route("/signout", "web/signout.tsx"),
-      route("/forgot-password", "web/forgot-password.tsx"),
-      route("/dashboard", "web/dashboard.tsx"),
+      route("signup", "web/signup.tsx"),
+      route("signin", "web/signin.tsx"),
+      route("signout", "web/signout.tsx"),
+      route("forgot-password", "web/forgot-password.tsx"),
+      route("dashboard", "web/dashboard.tsx"),
     ]),
 
     route("*", "web/not-found.tsx"), // Custom 404
   ]),
 
-  route("/api/auth/*", "web/api/auth.ts"),
-  route("/api/trpc/*", "web/api/trpc.ts"),
+  ...prefix("api", [
+    route("auth/*", "web/api/auth.ts"),
+    route("trpc/*", "web/api/trpc.ts"),
+  ]),
 
-  route("/action/set-theme", "web/action/set-theme.ts"),
-  route("/action/social", "web/action/social.ts"),
-  route("/action/example", "web/action/example.ts"),
+  ...prefix("action", [
+    route("set-theme", "web/action/set-theme.ts"),
+    route("social", "web/action/social.ts"),
+    route("example", "web/action/example.ts"),
+  ]),
 ] satisfies RouteConfig;
